7020a538a17de9c6db480476a89c04f2276d9ba8
sidhvip <117767084+sidhvip@users.noreply.github.com>
2024-03-10 00:20:04 +0530

    Update PES1UG21CS403-1.cpp

--- working.cpp
+++ working.cpp
@@ -1,35 +1,6 @@
-pipeline {
-    agent any
+#include <iostream>
 
-    stages {
-        stage('Build') {
-            steps { 
-                sh 'g++ PES1UG21CS403-1.cpp -o temp'
-                 build job: 'PES1UG21CS403-1', wait: false
-                 echo 'Build by CS403 successful'
-            }
-        }
-
-        stage('Test') {
-            steps {
-                sh 'cat PES1UG21CS403-1.cpp'
-                echo 'Test by CS403 successful'
-            }
-        }
-
-        stage('Deploy') {
-            steps {
-               
-                 echo 'Deploy by CS403 successful'
-            }
-        }
-    }
-
-    post {
-        failure {
-            
-                echo 'Pipeline Failed'
-          
-        }
-    }
+int main() {
+    std::cout << "My SRN is PES1UG21CS403" << std::endl;
+    return 0;
 }
